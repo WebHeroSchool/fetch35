d9e2d9b2671349bb5d8deb7505743e41b9318461
Mogilnik Valeria <lera___2000-29@mail.ru>
2020-10-29 17:40:05 +1000

    newscript

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const lastEl = document.getElementById('end');
 
 fetch ('https://api.github.com/users/variarmo')
   .then(res => res.json())
-  .then(json => {     
+  .then(json => {   
+    const body = document.body;
+    const lastEl = document.getElementById('end');
+
     let userLogin = document.createElement('h3');
     userLogin.innerHTML = json.login;
     body.insertBefore(userLogin, lastEl);
@@ -21,7 +24,10 @@ fetch ('https://api.github.com/users/variarmo')
     userAvatar.src = json.avatar_url;
     body.insertBefore(userAvatar, lastEl);
   })
-	.catch(err => {
+  
+  .catch(err => {
+    const body = document.body;
+    const lastEl = document.getElementById('end');
     let notFound = document.createElement('div')
     notFound.innerHTML = 'Информация о пользователе не доступна';
     body.insertBefore(notFound, lastEl);
